f42bf4f166f0fc2b76246ef5c5f3fd11697b385d
DrMack97 <delgadoantolinezdavidalejandro@gmail.com>
2026-04-10 14:01:45 +0200

    new

--- script.js
+++ script.js
@@ -1,24 +1,70 @@
 "use strict";
 // let prova:string = "hola";
-let button = document.getElementById('sendid');
-let input = document.querySelector('#sendid');
+/* para hacer la conversion:
+    - tsc .\script.ts -
+*/
+let button = document.getElementById('sendButtun');
+let input = document.querySelector('#inputid');
+let errorDiv = document.querySelector('#textError');
+let selectError = document.querySelector('#textError');
+let checkError = document.querySelector('#textError');
+let radioError = document.querySelector('#textError');
 button.addEventListener('click', () => {
-    let inputValue = document.getElementById('inputid')?.nodeValue;
+    //validar el campo de texto
+    let inputValue = input.value;
     //console.log(inputValue);
     if (inputValue.length < 4) {
-        console.log('Long error');
-        input?.classList.add('inputError');
+        input.classList.add('inputError');
+        selectError.style.display = "inline";
     }
     else {
-        //eviar form
+        input.classList.remove('inputError');
+        errorDiv.style.display = "none";
+        //enviar form
+    }
+    //validar el select 
+    let cicloElement = document.querySelector('#ciclo');
+    let ciclo = cicloElement.value;
+    console.log(cicloElement);
+    console.log(ciclo);
+    //if(ciclo === ""){}
+    if (ciclo) {
+        cicloElement.classList.add('inputError');
+        selectError.style.display = "inline";
+    }
+    else {
+        cicloElement.classList.remove('inputError');
+        selectError.style.display = "inline";
+    }
+    //validar el radioButton
+    let radioElement = (document.querySelector('input[name="genero"]:checked'));
+    //let radioValue = radioElement.value;
+    console.log(radioElement);
+    //console.log(radioValue);
+    if (!radioElement) {
+        radioError.style.display = "inline";
+    }
+    else {
+        radioError.style.display = "none";
+    }
+    //validar el checkbox
+    let checkElement = (document.querySelector('input[name="genero"]:checked'));
+    //let radioValue = radioElement.value;
+    console.log(checkElement);
+    //console.log(radioValue);
+    if (!radioElement) {
+        checkError.style.display = "inline";
+    }
+    else {
+        checkError.style.display = "none";
     }
 });
-let div1 = document.querySelector('#div1');
-div1.addEventListener('click', (event) => {
-    console.log(event);
-    console.log(event.target);
-    console.log(event.currentTarget);
-});
+// let div1:HTMLElement = (document.querySelector('#div1') as HTMLElement);
+// div1.addEventListener('click',(event)=> {
+//     console.log(event)
+//     console.log(event.target);
+//     console.log(event.currentTarget);
+// })
 /*function func1(param1:String,param2:number):string{
     return "hola";
 }
--- script.ts
+++ script.ts
@@ -1,27 +1,91 @@
 // let prova:string = "hola";
 
-let button: HTMLButtonElement | null = (document.getElementById('sendid') as HTMLButtonElement);
-let input: HTMLInputElement | null = (document.querySelector('#sendid') as HTMLInputElement);
+
+/* para hacer la conversion:
+    - tsc .\script.ts -
+*/
+
+let button: HTMLButtonElement  = (document.getElementById('sendButtun') as HTMLButtonElement);
+let input: HTMLInputElement = (document.querySelector('#inputid') as HTMLInputElement);
+let errorDiv:HTMLElement = (document.querySelector('#textError') as HTMLElement); 
+let selectError: HTMLElement = (document.querySelector('#textError') as HTMLElement); 
+let checkError: HTMLElement = (document.querySelector('#textError') as HTMLElement); 
+let radioError: HTMLElement = (document.querySelector('#textError') as HTMLElement); 
+
 
 button.addEventListener('click',()=>{
-    let inputValue: string | null = (document.getElementById('inputid') as HTMLInputElement)?.nodeValue;
+
+    //validar el campo de texto
+
+    let inputValue: string = input.value;
 
     //console.log(inputValue);
     if(inputValue.length < 4){
-        console.log('Long error');
-        input?.classList.add('inputError');
+
+        input.classList.add('inputError');
+        
+        selectError.style.display= "inline";
+        
     }else{
-        //eviar form
+        input.classList.remove('inputError')
+        errorDiv.style.display = "none";
+        //enviar form
     }
+
+    //validar el select 
+
+    let cicloElement:HTMLSelectElement = (document.querySelector('#ciclo') as HTMLSelectElement);
+    let ciclo = cicloElement.value;
+    console.log(cicloElement);
+    console.log(ciclo);
+
+    //if(ciclo === ""){}
+    
+    if(ciclo){
+        cicloElement.classList.add('inputError');
+        selectError.style.display = "inline";
+
+    }else{
+        cicloElement.classList.remove('inputError');
+        selectError.style.display = "inline";
+
+    }
+
+    //validar el radioButton
+
+        let radioElement: HTMLInputElement = (document.querySelector('input[name="genero"]:checked')) as HTMLInputElement;
+        //let radioValue = radioElement.value;
+        console.log(radioElement);
+        //console.log(radioValue);
+
+        if(!radioElement){
+            radioError.style.display = "inline";
+        }else{
+            radioError.style.display = "none";
+        }
+
+
+
+    //validar el checkbox
+        let checkElement: HTMLInputElement = (document.querySelector('input[name="genero"]:checked')) as HTMLInputElement;
+        //let radioValue = radioElement.value;
+        console.log(checkElement);
+        //console.log(radioValue);
+
+        if(!radioElement){
+            checkError.style.display = "inline";
+        }else{
+            checkError.style.display = "none";
+        }
 });
 
-let div1:HTMLElement = (document.querySelector('#div1') as HTMLElement);
+// let div1:HTMLElement = (document.querySelector('#div1') as HTMLElement);
 
-div1.addEventListener('click',(event)=> {
-    console.log(event)
-    console.log(event.target);
-    console.log(event.currentTarget);
-})
+// div1.addEventListener('click',(event)=> {
+//     console.log(event)
+//     console.log(event.target);
+//     console.log(event.currentTarget);
+// })
 
 /*function func1(param1:String,param2:number):string{
     return "hola";
